d5454bee5b7a5aa929fc7007c9b26cbf7c4877c8
guneevaidasani1 <ga24csb1a18@student.nitw.ac.in>
2025-10-22 15:59:49 +0530

    hopefully these are the final 3 datasets , replaced breakhis with uc merced

--- config.py
+++ config.py
@@ -1,52 +1,55 @@
 
 PROJECT_NAME = "HGAO-DenseNet Multi-Domain Classifier"
-SEED = 42            # For reproducibility
+SEED = 42             
 IMAGE_SIZE = (224, 224) 
-CHANNELS = 3         
-BATCH_SIZE = 32      
-
-
+CHANNELS = 3          
+BATCH_SIZE = 32       
 
 
 KAGGLE_DATASET_IDS = [
+   
+    "abdulhasibuddin/uc-merced-land-use-dataset",
     "jiayuanchengala/aid-scene-classification-datasets"
 ]
 
 
 DATASET_CONFIG = {
-    "BREAKHIS": {
-        "local_name": "dataset_cancer_v1", 
-        "num_classes": 4,       
+    "UCMERCED": {
+        "local_name": "UCMerced_LandUse", 
+        "num_classes": 21,       
+        "path_suffix": "Images", 
         "input_shape": IMAGE_SIZE + (CHANNELS,),
     },
     
-    
+
     "MED_WASTE": {
         "local_name": "Medical Waste 4.0", 
-        "num_classes": 13,       
+        "num_classes": 13, 
+        "path_suffix": "Medical Waste 4.0", 
         "input_shape": IMAGE_SIZE + (CHANNELS,),
     },
     
-    
+
     "AID": {
         "local_name": "AID", 
-        "num_classes": 30,      
+        "num_classes": 30,
+        "path_suffix": None, 
         "input_shape": IMAGE_SIZE + (CHANNELS,),
     },
     
-    "TARGET_DATASET": "BREAKHIS", 
+    "TARGET_DATASET": "UCMERCED", 
 }
 
 DATA_DIR = "./data/"
 
-TARGET_DATASET = DATASET_CONFIG["TARGET_DATASET"]
 
+TARGET_DATASET = DATASET_CONFIG["TARGET_DATASET"]
 
 HGAO_SEARCH_SPACE = {
     "learning_rate": [1e-5, 1e-1], 
-    "dropout_rate": [0.1, 0.6],    
-    "P": 30,             
-    "T": 10,             
+    "dropout_rate": [0.1, 0.6],     
+    "P": 30,              
+    "T": 10,              
     "beta1_weight": [0.1, 0.9], 
     "beta2_weight": [0.1, 0.9], 
 }
